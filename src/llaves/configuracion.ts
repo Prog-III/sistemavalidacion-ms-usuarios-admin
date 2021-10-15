ad8e0419a1804a14d71e674bcbb5d8e205876a0b
Albert Muñoz <albert.1701920351@ucaldas.edu.co>
2021-10-15 11:19:20 -0500

    Creación de carpeta llaves y configuracion de constantes

---
+++ src/llaves/configuracion.ts
@@ -0,0 +1,4 @@
+export namespace Configuracion {
+  export const hasNotificacion = "ZFQPuPGReSal"
+  export const mensajeCambioClave = "Su contraseña ha sido actualizada correctamente. Si no ha sido usted, la puede recuperar mediante la funcionalidad del sistema con su correo electrónico"
+}
