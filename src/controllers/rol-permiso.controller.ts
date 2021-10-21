6a70068d3633b74e2a28fc734f8d68b6cb2d9244
Albert Muñoz <albert.1701920351@ucaldas.edu.co>
2021-10-20 20:23:09 -0500

    Fixeado error de relacion N-N UsuarioRol y RolPermiso

--- src/controllers/rol-permiso.controller.ts
+++ src/controllers/rol-permiso.controller.ts
@@ -1,36 +1,30 @@
 import {
-  Count,
-  CountSchema,
   Filter,
-  repository,
-  Where,
+  repository
 } from '@loopback/repository';
-  import {
+import {
   del,
   get,
-  getModelSchemaRef,
-  getWhereSchemaFor,
-  param,
-  patch,
-  post,
+  getModelSchemaRef, param, post,
   requestBody,
+  response
 } from '@loopback/rest';
 import {
-Rol,
-PermisoRol,
-Permiso,
+  Permiso, PermisoRol, Rol
 } from '../models';
-import {RolRepository} from '../repositories';
+import {ArregloGeneral} from '../models/arreglo-general';
+import {PermisoRolRepository, RolRepository} from '../repositories';
 
 export class RolPermisoController {
   constructor(
     @repository(RolRepository) protected rolRepository: RolRepository,
+    @repository(PermisoRolRepository) protected permisoRolRepository: PermisoRolRepository
   ) { }
 
-  @get('/rols/{id}/permisos', {
+  @get('/roles/{id}/permisos', {
     responses: {
       '200': {
-        description: 'Array of Rol has many Permiso through PermisoRol',
+        description: 'Array of Roles has many Permiso through RolPermiso',
         content: {
           'application/json': {
             schema: {type: 'array', items: getModelSchemaRef(Permiso)},
@@ -46,65 +40,170 @@ export class RolPermisoController {
     return this.rolRepository.tiene_permisos(id).find(filter);
   }
 
-  @post('/rols/{id}/permisos', {
+  @post('/rol-permiso', {
     responses: {
       '200': {
-        description: 'create a Permiso model instance',
-        content: {'application/json': {schema: getModelSchemaRef(Permiso)}},
+        description: 'create a instance of permiso with a rol',
+        content: {'application/json': {schema: getModelSchemaRef(PermisoRol)}},
       },
     },
   })
-  async create(
-    @param.path.string('id') id: typeof Rol.prototype._id,
+  async createRelation(
     @requestBody({
       content: {
         'application/json': {
-          schema: getModelSchemaRef(Permiso, {
-            title: 'NewPermisoInRol',
+          schema: getModelSchemaRef(PermisoRol, {
+            title: 'NewPermisoWithRol',
             exclude: ['_id'],
           }),
         },
       },
-    }) permiso: Omit<Permiso, '_id'>,
-  ): Promise<Permiso> {
-    return this.rolRepository.tiene_permisos(id).create(permiso);
+    }) datos: Omit<PermisoRol, 'id'>,
+  ): Promise<PermisoRol | null> {
+    let registro = await this.permisoRolRepository.create(datos);
+    return registro;
   }
 
-  @patch('/rols/{id}/permisos', {
+  @post('/asociar-rol-permisos/{id}', {
     responses: {
       '200': {
-        description: 'Rol.Permiso PATCH success count',
-        content: {'application/json': {schema: CountSchema}},
+        description: 'create a instance of permisos with a rol',
+        content: {'application/json': {schema: getModelSchemaRef(PermisoRol)}},
       },
     },
   })
-  async patch(
-    @param.path.string('id') id: string,
+  async createRelations(
     @requestBody({
       content: {
         'application/json': {
-          schema: getModelSchemaRef(Permiso, {partial: true}),
+          schema: getModelSchemaRef(ArregloGeneral, {}),
         },
       },
-    })
-    permiso: Partial<Permiso>,
-    @param.query.object('where', getWhereSchemaFor(Permiso)) where?: Where<Permiso>,
-  ): Promise<Count> {
-    return this.rolRepository.tiene_permisos(id).patch(permiso, where);
+    }) datos: ArregloGeneral,
+    @param.path.string('id') id_rol: typeof Rol.prototype._id
+  ): Promise<Boolean> {
+    if (datos.arraygeneral.length > 0) {
+      datos.arraygeneral.forEach(async (id_permiso: string) => {
+        let existe = await this.permisoRolRepository.findOne({
+          where: {
+            id_rol: id_rol,
+            id_permiso: id_permiso
+          }
+        });
+        if (!existe) {
+          this.permisoRolRepository.create({
+            id_rol: id_rol,
+            id_permiso: id_permiso
+          });
+        }
+      });
+      return true;
+    }
+    return false;
   }
 
-  @del('/rols/{id}/permisos', {
-    responses: {
-      '200': {
-        description: 'Rol.Permiso DELETE success count',
-        content: {'application/json': {schema: CountSchema}},
+
+  @del('/roles/{id_rol}/{id_permiso}')
+  @response(204, {
+    description: 'relation DELETE success',
+  })
+  async EliminarPermisodeRol(
+    @param.path.string('id_rol') id_rol: string,
+    @param.path.string('id_permiso') id_permiso: string): Promise<Boolean> {
+    let reg = await this.permisoRolRepository.findOne({
+      where: {
+        id_rol: id_rol,
+        id_permiso: id_permiso
+      }
+    });
+    if (reg) {
+      await this.permisoRolRepository.deleteById(reg._id);
+      return true;
+    }
+    return false;
+  }
+
+}
+/* -------------------
+
+@get('/rols/{id}/permisos', {
+  responses: {
+    '200': {
+      description: 'Array of Rol has many Permiso through PermisoRol',
+      content: {
+        'application/json': {
+          schema: {type: 'array', items: getModelSchemaRef(Permiso)},
+        },
       },
     },
-  })
-  async delete(
+  },
+})
+async find(
     @param.path.string('id') id: string,
-    @param.query.object('where', getWhereSchemaFor(Permiso)) where?: Where<Permiso>,
-  ): Promise<Count> {
-    return this.rolRepository.tiene_permisos(id).delete(where);
-  }
+    @param.query.object('filter') filter ?: Filter < Permiso >,
+  ): Promise < Permiso[] > {
+  return this.rolRepository.tiene_permisos(id).find(filter);
 }
+
+@post('/rols/{id}/permisos', {
+  responses: {
+    '200': {
+      description: 'create a Permiso model instance',
+      content: {'application/json': {schema: getModelSchemaRef(Permiso)}},
+    },
+  },
+})
+async create(
+    @param.path.string('id') id: typeof Rol.prototype._id,
+    @requestBody({
+  content: {
+    'application/json': {
+      schema: getModelSchemaRef(Permiso, {
+        title: 'NewPermisoInRol',
+        exclude: ['_id'],
+      }),
+    },
+  },
+}) permiso: Omit < Permiso, '_id' >,
+  ): Promise < Permiso > {
+  return this.rolRepository.tiene_permisos(id).create(permiso);
+}
+
+@patch('/rols/{id}/permisos', {
+  responses: {
+    '200': {
+      description: 'Rol.Permiso PATCH success count',
+      content: {'application/json': {schema: CountSchema}},
+    },
+  },
+})
+async patch(
+    @param.path.string('id') id: string,
+    @requestBody({
+  content: {
+    'application/json': {
+      schema: getModelSchemaRef(Permiso, {partial: true}),
+    },
+  },
+})
+permiso: Partial < Permiso >,
+    @param.query.object('where', getWhereSchemaFor(Permiso)) where ?: Where < Permiso >,
+  ): Promise < Count > {
+  return this.rolRepository.tiene_permisos(id).patch(permiso, where);
+}
+
+@del('/rols/{id}/permisos', {
+  responses: {
+    '200': {
+      description: 'Rol.Permiso DELETE success count',
+      content: {'application/json': {schema: CountSchema}},
+    },
+  },
+})
+async delete (
+    @param.path.string('id') id: string,
+    @param.query.object('where', getWhereSchemaFor(Permiso)) where ?: Where < Permiso >,
+  ): Promise < Count > {
+  return this.rolRepository.tiene_permisos(id).delete(where);
+}
+}*/
--- src/controllers/usuario-rol.controller.ts
+++ src/controllers/usuario-rol.controller.ts
@@ -1,33 +1,27 @@
 import {
-  Count,
-  CountSchema,
   Filter,
-  repository,
-  Where,
+  repository
 } from '@loopback/repository';
-  import {
+import {
   del,
   get,
-  getModelSchemaRef,
-  getWhereSchemaFor,
-  param,
-  patch,
-  post,
+  getModelSchemaRef, param, post,
   requestBody,
+  response
 } from '@loopback/rest';
 import {
-Usuario,
-UsuarioRol,
-Rol,
+  Rol, Usuario, UsuarioRol
 } from '../models';
-import {UsuarioRepository} from '../repositories';
+import {ArregloGeneral} from '../models/arreglo-general';
+import {UsuarioRepository, UsuarioRolRepository} from '../repositories';
 
 export class UsuarioRolController {
   constructor(
     @repository(UsuarioRepository) protected usuarioRepository: UsuarioRepository,
+    @repository(UsuarioRolRepository) protected usuarioRolRepository: UsuarioRolRepository
   ) { }
 
-  @get('/usuarios/{id}/rols', {
+  @get('/usuarios/{id}/roles', {
     responses: {
       '200': {
         description: 'Array of Usuario has many Rol through UsuarioRol',
@@ -46,65 +40,88 @@ export class UsuarioRolController {
     return this.usuarioRepository.tiene_roles(id).find(filter);
   }
 
-  @post('/usuarios/{id}/rols', {
+  @post('/usuario-rol', {
     responses: {
       '200': {
-        description: 'create a Rol model instance',
-        content: {'application/json': {schema: getModelSchemaRef(Rol)}},
+        description: 'create a instance of rol with a usuario',
+        content: {'application/json': {schema: getModelSchemaRef(UsuarioRol)}},
       },
     },
   })
-  async create(
-    @param.path.string('id') id: typeof Usuario.prototype._id,
+  async createRelation(
     @requestBody({
       content: {
         'application/json': {
-          schema: getModelSchemaRef(Rol, {
-            title: 'NewRolInUsuario',
+          schema: getModelSchemaRef(UsuarioRol, {
+            title: 'NewRolWithUsuario',
             exclude: ['_id'],
           }),
         },
       },
-    }) rol: Omit<Rol, '_id'>,
-  ): Promise<Rol> {
-    return this.usuarioRepository.tiene_roles(id).create(rol);
+    }) datos: Omit<UsuarioRol, 'id'>,
+  ): Promise<UsuarioRol | null> {
+    let registro = await this.usuarioRolRepository.create(datos);
+    return registro;
   }
 
-  @patch('/usuarios/{id}/rols', {
+  @post('/asociar-usuario-roles/{id}', {
     responses: {
       '200': {
-        description: 'Usuario.Rol PATCH success count',
-        content: {'application/json': {schema: CountSchema}},
+        description: 'create a instance of roles with a usuario',
+        content: {'application/json': {schema: getModelSchemaRef(UsuarioRol)}},
       },
     },
   })
-  async patch(
-    @param.path.string('id') id: string,
+  async createRelations(
     @requestBody({
       content: {
         'application/json': {
-          schema: getModelSchemaRef(Rol, {partial: true}),
+          schema: getModelSchemaRef(ArregloGeneral, {}),
         },
       },
-    })
-    rol: Partial<Rol>,
-    @param.query.object('where', getWhereSchemaFor(Rol)) where?: Where<Rol>,
-  ): Promise<Count> {
-    return this.usuarioRepository.tiene_roles(id).patch(rol, where);
+    }) datos: ArregloGeneral,
+    @param.path.string('id') id_usuario: typeof Usuario.prototype._id
+  ): Promise<Boolean> {
+    if (datos.arraygeneral.length > 0) {
+      datos.arraygeneral.forEach(async (id_rol: string) => {
+        let existe = await this.usuarioRolRepository.findOne({
+          where: {
+            id_usuario: id_usuario,
+            id_rol: id_rol
+          }
+        });
+        if (!existe) {
+          this.usuarioRolRepository.create({
+            id_usuario: id_usuario,
+            id_rol: id_rol
+          });
+        }
+      });
+      return true;
+    }
+    return false;
   }
 
-  @del('/usuarios/{id}/rols', {
-    responses: {
-      '200': {
-        description: 'Usuario.Rol DELETE success count',
-        content: {'application/json': {schema: CountSchema}},
-      },
-    },
+
+  @del('/usuarios/{id_usuario}/{id_rol}')
+  @response(204, {
+    description: 'relation DELETE success',
   })
-  async delete(
-    @param.path.string('id') id: string,
-    @param.query.object('where', getWhereSchemaFor(Rol)) where?: Where<Rol>,
-  ): Promise<Count> {
-    return this.usuarioRepository.tiene_roles(id).delete(where);
+  async EliminarRoldeUsuario(
+    @param.path.string('id_usuario') id_usuario: string,
+    @param.path.string('id_rol') id_rol: string): Promise<Boolean> {
+    let reg = await this.usuarioRolRepository.findOne({
+      where: {
+        id_usuario: id_usuario,
+        id_rol: id_rol
+      }
+    });
+    if (reg) {
+      await this.usuarioRolRepository.deleteById(reg._id);
+      return true;
+    }
+    return false;
   }
+
 }
+/* ------------------- */
